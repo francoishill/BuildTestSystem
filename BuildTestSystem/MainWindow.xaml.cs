eff7e1b917f2c967a76f510f8254cf65797e38f8
Francois Hill <francoishill11@gmail.com>
2013-02-21 05:14:39 +0200

    Does not make sense to ask for "unpushed" changes as it will need to be compared against a specific remote.

--- BuildTestSystem/MainWindow.xaml.cs
+++ BuildTestSystem/MainWindow.xaml.cs
@@ -1025,8 +1025,6 @@ namespace BuildTestSystem
 
 		private void ContextmenuGitPush(object sender, RoutedEventArgs e)
 		{
-			//Figure out how to get committed but NOT PUSHED changes..
-
 			DoOperationWithApps(
 				   GetBuildAppList_FromContextMenu(sender),
 				   app =>
